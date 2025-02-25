12d75563f23e2e4b80e2477fe9c05b615a3e4737
Vladimir Polozov <volodya06061991@gmail.com>
2025-02-25 19:44:39 +0300

    Commit

---
+++ app/src/main/java/ru/netology/nmedia/view/MainActivity.kt
@@ -0,0 +1,71 @@
+package ru.netology.nmedia.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        applyInset(binding.main)
+        val viewModel: PostViewModel by viewModels ()
+       viewModel.data.observe(this){ post ->
+           with(binding) {
+               shareQuantity.text = CountCalculator.calculator(post.share)
+
+               share.setOnClickListener {
+                   viewModel.share()
+                   shareQuantity.text = CountCalculator.calculator(post.share)
+               }
+
+           }
+           with(binding) {
+               like.setImageResource(
+                   if (post.likedByMe) {
+                       R.drawable.baseline_favorite_24
+                   } else {
+                       R.drawable.baseline_favorite_border_24
+                   }
+               )
+
+               likeQuantity.text = CountCalculator.calculator(post.likes)
+
+
+               like.setOnClickListener {
+                   viewModel.like()
+
+
+                   likeQuantity.text = CountCalculator.calculator(post.likes)
+
+               }
+
+               content.text = post.content
+               author.text = post.author
+               published.text = post.published
+
+           }
+       }
+
+    }
+
+    private fun applyInset(main: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
